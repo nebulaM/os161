ac081ed42f4b76bb5b31a9c5464eb501e7b6a472
nebulam <nebulam12@gmail.com>
2017-04-03 00:04:43 -0700

    format intend

--- src/kern/thread/synch.c
+++ src/kern/thread/synch.c
@@ -51,23 +51,23 @@ sem_create(const char *name, unsigned initial_count)
 
         sem = kmalloc(sizeof(struct semaphore));
         if (sem == NULL) {
-                return NULL;
+            return NULL;
         }
 
         sem->sem_name = kstrdup(name);
         if (sem->sem_name == NULL) {
-                kfree(sem);
-                return NULL;
+        kfree(sem);
+            return NULL;
         }
 
-      	sem->sem_wchan = wchan_create(sem->sem_name);
-      	if (sem->sem_wchan == NULL) {
-      		kfree(sem->sem_name);
-      		kfree(sem);
-      		return NULL;
-      	}
+        sem->sem_wchan = wchan_create(sem->sem_name);
+        if (sem->sem_wchan == NULL) {
+            kfree(sem->sem_name);
+            kfree(sem);
+            return NULL;
+        }
 
-	      spinlock_init(&sem->sem_lock);
+        spinlock_init(&sem->sem_lock);
         sem->sem_count = initial_count;
 
         return sem;
@@ -78,9 +78,9 @@ sem_destroy(struct semaphore *sem)
 {
         KASSERT(sem != NULL);
 
-      	/* wchan_cleanup will assert if anyone's waiting on it */
-      	spinlock_cleanup(&sem->sem_lock);
-      	wchan_destroy(sem->sem_wchan);
+        /* wchan_cleanup will assert if anyone's waiting on it */
+        spinlock_cleanup(&sem->sem_lock);
+        wchan_destroy(sem->sem_wchan);
 
         kfree(sem->sem_name);
         kfree(sem);
@@ -92,33 +92,33 @@ P(struct semaphore *sem)
         KASSERT(sem != NULL);
 
         /*
-         * May not block in an interrupt handler.
-         *
-         * For robustness, always check, even if we can actually
-         * complete the P without blocking.
-         */
+        * May not block in an interrupt handler.
+        *
+        * For robustness, always check, even if we can actually
+        * complete the P without blocking.
+        */
         KASSERT(curthread->t_in_interrupt == false);
 
-	      /* Use the semaphore spinlock to protect the wchan as well. */
-	      spinlock_acquire(&sem->sem_lock);
+        /* Use the semaphore spinlock to protect the wchan as well. */
+        spinlock_acquire(&sem->sem_lock);
         while (sem->sem_count == 0) {
-        		/*
-        		 *
-        		 * Note that we don't maintain strict FIFO ordering of
-        		 * threads going through the semaphore; that is, we
-        		 * might "get" it on the first try even if other
-        		 * threads are waiting. Apparently according to some
-        		 * textbooks semaphores must for some reason have
-        		 * strict ordering. Too bad. :-)
-        		 *
-        		 * Exercise: how would you implement strict FIFO
-        		 * ordering?
-        		 */
-        		wchan_sleep(sem->sem_wchan, &sem->sem_lock);
+            /*
+            *
+            * Note that we don't maintain strict FIFO ordering of
+            * threads going through the semaphore; that is, we
+            * might "get" it on the first try even if other
+            * threads are waiting. Apparently according to some
+            * textbooks semaphores must for some reason have
+            * strict ordering. Too bad. :-)
+            *
+            * Exercise: how would you implement strict FIFO
+            * ordering?
+            */
+            wchan_sleep(sem->sem_wchan, &sem->sem_lock);
         }
         KASSERT(sem->sem_count > 0);
         sem->sem_count--;
-	      spinlock_release(&sem->sem_lock);
+        spinlock_release(&sem->sem_lock);
 }
 
 void
@@ -126,13 +126,13 @@ V(struct semaphore *sem)
 {
         KASSERT(sem != NULL);
 
-	      spinlock_acquire(&sem->sem_lock);
+        spinlock_acquire(&sem->sem_lock);
 
         sem->sem_count++;
         KASSERT(sem->sem_count > 0);
-	      wchan_wakeone(sem->sem_wchan, &sem->sem_lock);
+        wchan_wakeone(sem->sem_wchan, &sem->sem_lock);
 
-	      spinlock_release(&sem->sem_lock);
+        spinlock_release(&sem->sem_lock);
 }
 
 ////////////////////////////////////////////////////////////
@@ -161,15 +161,9 @@ lock_create(const char *name)
             return NULL;
         }
 
+        lock->owner = NULL;
         spinlock_init(&lock->spinlock);
         lock->is_held = false;
-
-        lock->owner = NULL;
-        if(lock->owner){
-            lock->owner = NULL;
-            lock_destroy(lock);
-            return NULL;
-        }
 
         return lock;
 }
